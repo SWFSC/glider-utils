4549fd949575be7189cea4d4fdb2e4975aeb190e
Sam Woodman <sam.woodman@noaa.gov>
2024-09-29 18:49:17 +0000

    Working on imagery metadata

--- esdglider/metadata.py
+++ esdglider/metadata.py
@@ -27,34 +27,35 @@ def solocam_filename_dt(filename, index_dt, format='%Y%m%d-%H%M%S'):
     return solocam_dt
 
 
-def imagery_metadata(ds, imagery_path, ext = 'jpg'):
+def imagery_metadata(ds_eng, ds_sci, imagery_dir, ext = 'jpg'):
     """
     Matches up imagery files with data from gdm object by imagery filename
     Uses interpolated variables (hardcoded in function)
     Returns data frame with metadata information
     
     Args:
-        ds (Dataset): xarray Dataset; from timeseries NetCDF
-        imagery_path (str): path to folder with images, 
+        ds_eng (Dataset): xarray Dataset; from engineering timeseries NetCDF
+        ds_sci (Dataset): xarray Dataset; from science timeseries NetCDF
+        imagery_dir (str): path to folder with images, 
             specifically the 'Dir####' folders
-        ext (str, optional): Imagery file extension. Defaults to 'jpg'.
+        ext (str, optional): Imagery file extension. Default is 'jpg'.
 
     Returns:
-        DataFrame: DataFrame of imagery metadata
+        DataFrame: pd.DataFrame of imagery metadata
     """
     
-    deployment = imagery_path.split("/")[-2]
+    deployment = imagery_dir.split("/")[-2]
     _log.info(f'Creating imagery metadata file for {deployment}')
-    _log.info(f"Creating from images path {imagery_path}")
+    _log.info(f"Using images directory {imagery_dir}")
 
     #--------------------------------------------
     # Checks
-    if not os.path.isdir(imagery_path):
-        _log.error(f'imagery_path ({imagery_path}) does not exist. ' + 
+    if not os.path.isdir(imagery_dir):
+        _log.error(f'imagery_dir ({imagery_dir}) does not exist. ' + 
                    'The imagery metadata file will not be created')
-        raise FileNotFoundError(f'Could not find {imagery_path}')
+        raise FileNotFoundError(f'Could not find {imagery_dir}')
     else:
-        filepaths = glob.glob(f'{imagery_path}/**/*.{ext}', recursive=True)
+        filepaths = glob.glob(f'{imagery_dir}/**/*.{ext}', recursive=True)
         _log.debug(f"Found {len(filepaths)} files with the extension {ext}")
         if len(filepaths) == 0:
             _log.error("Zero image files were found. Did you provide " +
@@ -64,7 +65,7 @@ def imagery_metadata(ds, imagery_path, ext = 'jpg'):
         imagery_files.sort()
 
     #--------------------------------------------
-    # Extract info from imagery file names, and match up with glider data
+    # Extract info from imagery file names
     _log.debug("Processing imagery file names")
 
     # Check that all filenames have the same number of characters
@@ -86,24 +87,66 @@ def imagery_metadata(ds, imagery_path, ext = 'jpg'):
                    f'and thus the imagery metadata will not be created')
         raise ValueError('Datetimes could not be extracted from imagery filenames')
 
-    df = pd.DataFrame(data={'img_file': imagery_files}, index=imagery_files_dt)
+    df = pd.DataFrame(data={'img_file': imagery_files, 'img_time': imagery_files_dt})
     # df.to_csv("/home/sam_woodman_noaa_gov/test.csv", index_label="time")
 
-    _log.debug("Interpolating glider data")
-    ds_interp = ds.interp(time=df.index.values)
-    df['latitude']  = ds_interp['latitude'].values
-    df['longitude'] = ds_interp['longitude'].values
-    df['depth']     = ds_interp['depth'].values
-    df['heading']   = ds_interp['heading'].values
-    df['pitch']     = ds_interp['pitch'].values
-    df['roll']      = ds_interp['roll'].values
-    
-    # TODO: bring in science data. so probs science timeseries?
-
     #--------------------------------------------
-    csv_file = os.path.join(imagery_path.replace("images", "metadata"), 
+    # Create metadata file
+    _log.debug("Extracting profile values")
+    profile_times = (ds_eng
+                 .to_pandas()
+                 .reset_index(names='time')
+                 .groupby(['profile_index', 'profile_direction'])
+                 .agg(min_time=('time', 'min'), max_time=('time', 'max'))
+                 .reset_index()
+    )
+    # Option 1: https://stackoverflow.com/a/44601120
+    # a = df.time.values
+    # bh = profile_times.max_time.values
+    # bl = profile_times.min_time.values
+    # i, j = np.where((a[:, None] >= bl) & (a[:, None] <= bh))
+    # d = pd.concat([
+    #     df.loc[i, :].reset_index(drop=True),
+    #     profile_times.loc[j, :].reset_index(drop=True)
+    # ], axis=1)
+
+    # Option 2: Chat
+    # a = df.copy()
+    # b = profile_times.copy()
+    # a['key'] = 0  # Add a dummy key to cross join
+    # b['key'] = 0  # Add a dummy key for merging
+    # # Perform a cross-join and filter by intervals
+    # merged = pd.merge(b, a, on='key').drop('key', axis=1)
+    # matches = merged[(merged['img_time'] >= merged['min_time']) & (merged['img_time'] <= merged['max_time'])]
+    # print(matches[['time', 'category1', 'category2']])
+
+    _log.debug("Interpolating engineering glider data")
+    ds_eng_interp = ds_eng.interp(time=df.img_time.values)
+    df['latitude']  = ds_eng_interp['latitude'].values
+    df['longitude'] = ds_eng_interp['longitude'].values
+    df['depth']     = ds_eng_interp['depth'].values
+    df['heading']   = ds_eng_interp['heading'].values
+    df['pitch']     = ds_eng_interp['pitch'].values
+    df['roll']      = ds_eng_interp['roll'].values
+    
+    _log.debug("Interpolating science glider data")
+    ds_sci_interp = ds_sci.interp(time=df.img_time.values)
+    df['conductivity']         = ds_sci_interp['conductivity'].values
+    df['temperature']          = ds_sci_interp['temperature'].values
+    df['pressure']             = ds_sci_interp['pressure'].values
+    df['salinity']             = ds_sci_interp['salinity'].values
+    df['density']              = ds_sci_interp['density'].values
+    df['depth_ctd']            = ds_sci_interp['depth_ctd'].values
+    df['oxygen_concentration'] = ds_sci_interp['oxygen_concentration'].values
+    df['chlorophyll']          = ds_sci_interp['chlorophyll'].values
+    df['cdom']                 = ds_sci_interp['cdom'].values
+    df['backscatter_700']      = ds_sci_interp['backscatter_700'].values
+    
+    #--------------------------------------------
+    # Export metadata file
+    csv_file = os.path.join(imagery_dir.replace("images", "metadata"), 
                             f'{deployment}-imagery-metadata.csv')
     _log.info(f'Writing imagery metadata to: {csv_file}')
-    df.to_csv(csv_file, index_label="time")
+    df.to_csv(csv_file, index=False)
 
     return df
--- scripts/smw-test.py
+++ scripts/smw-test.py
@@ -29,21 +29,23 @@ def main():
     utils.gcs_mount_bucket("amlr-gliders-deployments-dev", deployments_path, ro=True)
     glider_path = os.path.join(deployments_path, 'SANDIEGO', '2022', deployment)
     tsdir = os.path.join(glider_path, 'data', 'nc', 'timeseries')
-    outname_tseng = os.path.join(tsdir, os.listdir(tsdir)[0])
+    outname_dseng = os.path.join(tsdir, os.listdir(tsdir)[0])
+    outname_dssci = os.path.join(tsdir, os.listdir(tsdir)[1])
 
     img_path = "/home/sam_woodman_noaa_gov/amlr-gliders-imagery-raw-dev"
     utils.gcs_mount_bucket("amlr-gliders-imagery-raw-dev", img_path, ro=True)
-    x_path = os.path.join(img_path, "SANDIEGO", "2022", "amlr08-20220513", "images")
-    x_fn = os.listdir(os.path.join(x_path, "Dir0002"))
-    print(x_fn[0:5])
+    i_path = os.path.join(img_path, "SANDIEGO", "2022", "amlr08-20220513", "images")
+    i_fn = os.listdir(os.path.join(i_path, "Dir0002"))
+    print(i_fn[0:5])
 
 
     #-------------------------
     for i in range(0, 5):
-        print(met.solocam_filename_dt(x_fn[i], 5))
+        print(met.solocam_filename_dt(i_fn[i], 5))
 
-    tseng = xr.open_dataset(outname_tseng)
-    met.imagery_metadata(tseng, x_path)
+    dseng = xr.open_dataset(outname_dseng)
+    dssci = xr.open_dataset(outname_dssci)
+    met.imagery_metadata(dseng, dssci, i_path)
 
 if __name__ == "__main__":
     main()
