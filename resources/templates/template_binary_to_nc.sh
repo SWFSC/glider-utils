ba95612283233675a5b3c563427212432d50805c
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2024-11-07 12:08:47 -0800

    Update and reorg templates

---
+++ resources/templates/template_binary_to_nc.sh
@@ -0,0 +1,15 @@
+# # NOTE: This template script is still in development
+
+
+#!/bin/bash
+
+DEPLOYMENTS_PATH=/mnt/amlr-gliders-deployments-dev
+IMAGERY_PATH=/mnt/amlr-gliders-imagery-raw-dev
+
+# fusermount -u $DEPLOYMENTS_PATH
+gcsfuse --implicit-dirs amlr-gliders-deployments-dev $DEPLOYMENTS_PATH
+gcsfuse --implicit-dirs amlr-gliders-imagery-raw-dev $IMAGERY_PATH
+echo -e "\nend of bucket mounting\n"
+
+# Path on glider-proc VM
+/opt/conda-envs/esdglider/bin/python glider-utils/scripts/binary_to_nc.py amlr08-20220513 SANDIEGO delayed $PATH_DEPLOYMENTS --loglevel=info --write_timeseries --write_gridded
---
+++ resources/templates/template_scrape_sfmc.sh
@@ -0,0 +1,14 @@
+# # In glider-proc VM:
+# sudo touch /opt/glider-scripts/scrape-sfmc.sh
+# sudo chmod 775 /opt/glider-scripts
+# sudo chown -R gliders /opt/glider-scripts
+# sudo chgrp -R gliders /opt/glider-scripts
+# # Use nano, or other to paste in and update template code below
+
+
+#!/bin/bash
+
+# Scrape SFMC for definied deployment/project, using esdglider conda env
+DEPLOYMENT=calanus-20241019
+PROJECT=ECOSWIM
+/opt/conda-envs/esdglider/bin/python /opt/glider-utils/scripts/scrape-sfmc.py $DEPLOYMENT $PROJECT --loglevel=info --logfile=/opt/glider-scripts/$DEPLOYMENT-scrape-sfmc.log
