22bc54b8514bc6fbcc3c44301c047e73e0bd05e7
Sam Woodman <sam.woodman@noaa.gov>
2025-03-05 01:44:00 +0000

    testing script

--- scripts/smw-test.py
+++ scripts/smw-test.py
@@ -4,6 +4,7 @@ import os
 import logging
 import xarray as xr
 
+import esdglider.gcp as gcp
 import esdglider.utils as utils
 import esdglider.metadata as met
 import esdglider.process as process
@@ -20,42 +21,65 @@ def scrape_sfmc():
 
 
 def ts():
-    deployment = 'amlr08-20220513'
+    deployment = 'calanus-20241019'
+    project = "ECOSWIM"
     mode = 'delayed'
     bucket_name = 'amlr-gliders-deployments-dev'
 
     deployments_path = f'/home/sam_woodman_noaa_gov/{bucket_name}'
-    utils.gcs_mount_bucket("amlr-gliders-deployments-dev", deployments_path, ro=True)
-    glider_path = os.path.join(deployments_path, 'SANDIEGO', '2022', deployment)
-    tsdir = os.path.join(glider_path, 'data', 'nc', 'timeseries')
-    outname_dseng = os.path.join(tsdir, os.listdir(tsdir)[0])
-    outname_dssci = os.path.join(tsdir, os.listdir(tsdir)[1])
+    gcp.gcs_mount_bucket("amlr-gliders-deployments-dev", deployments_path, 
+                         ro=False)
 
-    img_path = "/home/sam_woodman_noaa_gov/amlr-gliders-imagery-raw-dev"
-    utils.gcs_mount_bucket("amlr-gliders-imagery-raw-dev", img_path, ro=True)
-    i_path = os.path.join(img_path, "SANDIEGO", "2022", "amlr08-20220513", "images")
-    i_fn = os.listdir(os.path.join(i_path, "Dir0002"))
-    print(i_fn[0:5])
+    x = process.binary_to_nc(deployment, project, mode, deployments_path, 
+                             write_timeseries=True, write_gridded=True)
+    
+    return x
+
+    # glider_path = os.path.join(deployments_path, 'SANDIEGO', '2022', deployment)
+    # tsdir = os.path.join(glider_path, 'data', 'nc', 'timeseries')
+    # outname_dseng = os.path.join(tsdir, os.listdir(tsdir)[0])
+    # outname_dssci = os.path.join(tsdir, os.listdir(tsdir)[1])
+
+    # img_path = "/home/sam_woodman_noaa_gov/amlr-gliders-imagery-raw-dev"
+    # utils.gcs_mount_bucket("amlr-gliders-imagery-raw-dev", img_path, ro=True)
+    # i_path = os.path.join(img_path, "SANDIEGO", "2022", "amlr08-20220513", "images")
+    # i_fn = os.listdir(os.path.join(i_path, "Dir0002"))
+    # print(i_fn[0:5])
 
 
-    #-------------------------
-    for i in range(0, 5):
-        print(met.solocam_filename_dt(i_fn[i], 5))
+    # #-------------------------
+    # for i in range(0, 5):
+    #     print(met.solocam_filename_dt(i_fn[i], 5))
 
-    dseng = xr.open_dataset(outname_dseng)
-    dssci = xr.open_dataset(outname_dssci)
-    met.imagery_metadata(dseng, dssci, i_path)
+    # dseng = xr.open_dataset(outname_dseng)
+    # dssci = xr.open_dataset(outname_dssci)
+    # met.imagery_metadata(dseng, dssci, i_path)
 
 if __name__ == "__main__":
     logging.basicConfig(
-        format='%(module)s:%(levelname)s:%(message)s [line %(lineno)d]', 
-        level=logging.INFO)
+        format='%(module)s:%(asctime)s:%(levelname)s:%(message)s [line %(lineno)d]', 
+        level=logging.INFO, 
+        datefmt='%Y-%m-%d %H:%M:%S')
     # logging.basicConfig(filename=logf,
     #                 filemode='w',
     #                 format='%(asctime)s %(levelname)-8s %(message)s',
     #                 level=logging.INFO,
     #                 datefmt='%Y-%m-%d %H:%M:%S')
 
-    # ts()
+    
     logging.info("Starting test")
-    scrape_sfmc()
+    # scrape_sfmc()
+    # print(ts())
+    outname_tseng, outname_tssci, outname_1m, outname_5m = ts()
+
+    print(outname_tseng)
+
+    ds_eng = xr.open_dataset(outname_tseng)
+    ds_sci = xr.open_dataset(outname_tssci)
+
+    print(f"there are {len(ds_eng.time)} points in the engineering timeseries")
+    print(f"there are {len(ds_sci.time)} points in the science timeseries")
+    # pd_eng = ds_eng.to_pandas()[['depth', 'profile_index', 'profile_direction']]
+    # pd_eng
+
+    gcp.gcs_unmount_bucket("amlr-gliders-deployments-dev")
