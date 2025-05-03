ca1bc3caf3133bd651cafb106714be58e8e65e3a
Sam Woodman <sam.woodman@noaa.gov>
2025-05-03 00:24:49 +0000

    use dbdreader to do some decompressing

---
+++ scripts/decompress.py
@@ -0,0 +1,49 @@
+import logging
+import os
+from dbdreader.decompress import is_compressed, decompress_file
+
+from esdglider import gcp, glider
+
+"""
+This script is intended to help users quickly generate decompressed 
+binary files, as a light wrapper around dbdreader functions.
+"""
+
+
+
+deployment_info = {
+    "deployment": "stenella-20250414",
+    "project": "SANDIEGO",
+    "mode": "delayed",
+    # "min_dt": "2023-11-28 20:46",
+}
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(module)s:%(asctime)s:%(levelname)s:%(message)s [line %(lineno)d]",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    bucket_name = "amlr-gliders-deployments-dev"
+    deployments_path = os.path.join("/home/sam_woodman_noaa_gov", bucket_name)
+    gcp.gcs_mount_bucket(bucket_name, deployments_path, ro=False)
+
+    paths = glider.get_path_deployment(deployment_info, deployments_path, "")
+
+    binarydir = paths["binarydir"]
+    binarydir_files = os.listdir(binarydir)
+    logging.info("There are %s files in %s", len(binarydir_files), binarydir)
+
+    # FileDecompressor.decompress(dcd1)
+    logging.info("decompressing all files in %s", binarydir)
+    for fin in binarydir_files:
+        logging.debug(fin)
+        if is_compressed(fin):
+            decompress_file(os.path.join(binarydir, fin))
+        else:
+            logging.debug("skipping %s", fin)
+
+
+    binarydir_files = os.listdir(binarydir)
+    logging.info("There are now %s files in %s", len(binarydir_files), binarydir)
