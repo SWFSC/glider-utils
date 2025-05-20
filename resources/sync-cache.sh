46236deffe648aef46e24d4be5adbc5fe88c34ad
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2025-05-20 14:50:57 -0700

    update metadata defaults

--- resources/sync-cache.sh
+++ resources/sync-cache.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 gcloud storage rsync gs://amlr-gliders-deployments-dev/cache ~/glider-utils/resources/example-data/cache
+
+# Or, from a local computer:
+# gcloud storage rsync C:\SMW\Gliders_Moorings\Gliders\Standard-glider-files\Cache gs://amlr-gliders-deployments-dev/cache
